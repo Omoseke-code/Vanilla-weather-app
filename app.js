a975e27046a012d7d390cb9d16d36d9aaa0ef7ef
Omoseke-code <111017844+Omoseke-code@users.noreply.github.com>
2022-12-03 11:02:47 +0100

    Worked on temperature conversion

--- app.js
+++ app.js
@@ -44,13 +44,16 @@ function firstForecast(response) {
   let wind = document.querySelector("#wind");
   let date = document.querySelector("#time");
   let icon = document.querySelector("#topicon");
+
+  celsiusTemp = response.data.daily[0].temperature.day;
+
   icon.setAttribute("src", response.data.daily[0].condition.icon_url);
   date.innerHTML = formatDate(response.data.daily[0].time * 1000);
   forecast.innerHTML = response.data.daily[0].condition.description;
   cityElement.innerHTML = response.data.city;
   wind.innerHTML = Math.round(response.data.daily[0].wind.speed);
   humidity.innerHTML = response.data.daily[0].temperature.humidity;
-  tempElement.innerHTML = Math.round(response.data.daily[0].temperature.day);
+  tempElement.innerHTML = Math.round(celsiusTemp);
 }
 function secondForecast(response) {
   let temp = document.querySelector("#temp1");
@@ -98,6 +101,26 @@ function handlesubmit(event) {
   let cityInput = document.querySelector("#city-input");
   search(cityInput.value);
 }
+function showFahrenheit(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#temp");
+
+  //remove active class from celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let f = (celsiusTemp * 9) / 5 + 12;
+  temp.innerHTML = Math.round(f);
+}
+function showCelsius(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#temp");
+
+  //add active class from celsius link
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+
+  temp.innerHTML = Math.round(celsiusTemp);
+}
 let apiKey = "2cf2304b8354b8ea17a2e89be11364ot";
 let city = "Lagos";
 let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=2cf2304b8354b8ea17a2e89be11364ot&units=metric`;
@@ -108,3 +131,12 @@ axios.get(apiUrl).then(fourthForecast);
 axios.get(apiUrl).then(fifthForecast);
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handlesubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsius);
+
+let celsiusTemp = null;
+//search("Lagos")
