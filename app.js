83cb4623d4c038e77c38c39d3452843dc7bd776a
Omoseke-code <111017844+Omoseke-code@users.noreply.github.com>
2022-12-03 09:25:26 +0100

    Integrated searches

--- app.js
+++ app.js
@@ -22,13 +22,29 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-function firstTemperature(response) {
+function formatFutureDates(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}`;
+}
+function firstForecast(response) {
   let tempElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let forecast = document.querySelector("#forecast");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
   let date = document.querySelector("#time");
+  let icon = document.querySelector("#topicon");
+  icon.setAttribute("src", response.data.daily[0].condition.icon_url);
   date.innerHTML = formatDate(response.data.daily[0].time * 1000);
   forecast.innerHTML = response.data.daily[0].condition.description;
   cityElement.innerHTML = response.data.city;
@@ -36,7 +52,59 @@ function firstTemperature(response) {
   humidity.innerHTML = response.data.daily[0].temperature.humidity;
   tempElement.innerHTML = Math.round(response.data.daily[0].temperature.day);
 }
+function secondForecast(response) {
+  let temp = document.querySelector("#temp1");
+  let date = document.querySelector("#date1");
+  let icon = document.querySelector("#icon1");
+  icon.setAttribute("src", response.data.daily[1].condition.icon_url);
+  date.innerHTML = formatFutureDates(response.data.daily[1].time * 1000);
+  temp.innerHTML = Math.round(response.data.daily[1].temperature.day);
+}
+function thirdForecast(response) {
+  let temp = document.querySelector("#temp2");
+  let date = document.querySelector("#date2");
+  let icon = document.querySelector("#icon2");
+  icon.setAttribute("src", response.data.daily[2].condition.icon_url);
+  date.innerHTML = formatFutureDates(response.data.daily[2].time * 1000);
+  temp.innerHTML = Math.round(response.data.daily[2].temperature.day);
+}
+function fourthForecast(response) {
+  let temp = document.querySelector("#temp3");
+  let date = document.querySelector("#date3");
+  let icon = document.querySelector("#icon3");
+  icon.setAttribute("src", response.data.daily[3].condition.icon_url);
+  date.innerHTML = formatFutureDates(response.data.daily[3].time * 1000);
+  temp.innerHTML = Math.round(response.data.daily[3].temperature.day);
+}
+function fifthForecast(response) {
+  let temp = document.querySelector("#temp4");
+  let date = document.querySelector("#date4");
+  let icon = document.querySelector("#icon4");
+  icon.setAttribute("src", response.data.daily[4].condition.icon_url);
+  date.innerHTML = formatFutureDates(response.data.daily[4].time * 1000);
+  temp.innerHTML = Math.round(response.data.daily[4].temperature.day);
+}
+function search(city) {
+  let apiKey = "2cf2304b8354b8ea17a2e89be11364ot";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=2cf2304b8354b8ea17a2e89be11364ot&units=metric`;
+  axios.get(apiUrl).then(firstForecast);
+  axios.get(apiUrl).then(secondForecast);
+  axios.get(apiUrl).then(thirdForecast);
+  axios.get(apiUrl).then(fourthForecast);
+  axios.get(apiUrl).then(fifthForecast);
+}
+function handlesubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
 let apiKey = "2cf2304b8354b8ea17a2e89be11364ot";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/forecast?query=Lagos&key=2cf2304b8354b8ea17a2e89be11364ot&units=metric";
-axios.get(apiUrl).then(firstTemperature);
+let city = "Lagos";
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=2cf2304b8354b8ea17a2e89be11364ot&units=metric`;
+axios.get(apiUrl).then(firstForecast);
+axios.get(apiUrl).then(secondForecast);
+axios.get(apiUrl).then(thirdForecast);
+axios.get(apiUrl).then(fourthForecast);
+axios.get(apiUrl).then(fifthForecast);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handlesubmit);
